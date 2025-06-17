fc09f19c14e9ede6249f497e3c413a484a7de0c1
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-17 09:48:16 -0300

    leetcode 9 without converting to str

--- leetcode/009.ts
+++ leetcode/009.ts
@@ -2,18 +2,36 @@
 // https://leetcode.com/problems/palindrome-number/description/
 
 // My initial solution
-function isPalindrome(x: number): boolean {
-  const xStr = x.toString()
-  const xArr: string[] = []
+// function isPalindrome(x: number): boolean {
+//   const xStr = x.toString()
+//   const xArr: string[] = []
 
-  for (let i = xStr.length - 1; i >= 0; i--) {
-    xArr.push(xStr[i])
-  }
+//   for (let i = xStr.length - 1; i >= 0; i--) {
+//     xArr.push(xStr[i])
+//   }
 
-  return xArr.join("") === xStr
-}
+//   return xArr.join("") === xStr
+// }
 
 // Follow up: Could you solve it without converting the integer to a string?
+function isPalindrome(x: number): boolean {
+  if (x < 0) return false
+  if (x < 10) return true
+
+  let reversed = 0
+  let remainder = x
+  const digits = Math.floor(Math.log10(x) + 1)
+
+  for (let i = digits; i > 0; i--) {
+    const lastDigit = remainder % 10
+    reversed += lastDigit * 10 ** (i - 1)
+    remainder = (remainder - lastDigit) / 10
+  }
+
+  return x === reversed
+}
+// I think it was a creative solution without converting the int to string
+// but it's less performatic than the initial solution
 
 console.log(isPalindrome(121)) // true
 console.log(isPalindrome(-121)) // false
