ad81765335fb837924ea902253a14047c0a14736
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-17 18:30:33 -0300

    add leetcode 9 best solution

--- leetcode/009.ts
+++ leetcode/009.ts
@@ -13,6 +13,8 @@
 //   return xArr.join("") === xStr
 // }
 
+// ############################################
+
 // Follow up: Could you solve it without converting the integer to a string?
 function isPalindrome(x: number): boolean {
   if (x < 0) return false
@@ -32,6 +34,24 @@ function isPalindrome(x: number): boolean {
 }
 // I think it was a creative solution without converting the int to string
 // but it's less performatic than the initial solution
+
+// ############################################
+
+// best and more performatic solution from leetcode:
+// function isPalindrome(x: number): boolean {
+//   if (x < 0) return false
+
+//   let xCopy = x
+//   let total = 0
+
+//   while (xCopy > 0) {
+//     const remainder = xCopy % 10
+//     total = total * 10 + remainder
+//     xCopy = Math.floor(xCopy / 10)
+//   }
+
+//   return x === total
+// }
 
 console.log(isPalindrome(121)) // true
 console.log(isPalindrome(-121)) // false
