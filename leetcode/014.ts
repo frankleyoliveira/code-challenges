ba4a78980d8df9a6b2bb84bf3d4154a7695efc10
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-20 11:25:05 -0300

    complete leetcode 14

---
+++ leetcode/014.ts
@@ -0,0 +1,24 @@
+// 14. Longest Common Prefix
+// https://leetcode.com/problems/longest-common-prefix/description/
+
+function longestCommonPrefix(strs: string[]): string {
+  let prefix = ''
+  const sortedStrs = strs.sort((a, b) => a.length - b.length)
+  const strsToCompare = sortedStrs.splice(1)
+  const shortestStr = sortedStrs.join('')
+
+  for (let i = 0; i < shortestStr.length; i++) {
+    let shortestStrPrefix = shortestStr[i]
+
+    if (!strsToCompare.every((str) => str[i] === shortestStrPrefix)) {
+      return prefix
+    }
+
+    prefix += shortestStrPrefix
+  }
+
+  return prefix
+}
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight'])) // fl
+console.log(longestCommonPrefix(['dog', 'racecar', 'car'])) // ""
