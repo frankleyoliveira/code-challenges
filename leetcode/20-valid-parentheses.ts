9aceeae7850fae872e1921de4fe246ba292b316b
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-21 15:48:14 -0300

    complete leetcode 20

---
+++ leetcode/20-valid-parentheses.ts
@@ -0,0 +1,50 @@
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/description/
+
+// My first solution
+// function isValidx(s: string): boolean {
+//   const pOpen = ['(', '[', '{']
+//   const pClose = [')', ']', '}']
+//   const stack: number[] = []
+
+//   for (const c of s) {
+//     const openIdx = pOpen.findIndex((p) => p === c)
+//     if (openIdx !== -1) {
+//       stack.push(openIdx)
+//     } else {
+//       const closeIdx = pClose.findIndex((p) => p === c)
+//       if (stack.pop() !== closeIdx) {
+//         return false
+//       }
+//     }
+//   }
+
+//   return stack.length === 0
+// }
+
+// refactored version
+function isValid(s: string): boolean {
+  const pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  }
+  const stack: ('(' | '[' | '{')[] = []
+
+  for (const c of s) {
+    if (c in pairs) {
+      // @ts-ignore
+      stack.push(c)
+    } else {
+      const key = stack.pop()
+      if (!key || pairs[key] !== c) return false
+    }
+  }
+
+  return stack.length === 0
+}
+
+console.log(isValid('()')) // true
+console.log(isValid('()[]{}')) // true
+console.log(isValid('(]')) // false
+console.log(isValid('([])')) // true
