31f14a53c621a2c75c723656b45637904d14d99e
Frankley Oliveira <frankleyj.o@gmail.com>
2026-06-28 20:26:50 -0300

    start 49

---
+++ typescript/leetcode/49-group-anagrams.ts
@@ -0,0 +1,10 @@
+// 49. Group Anagrams
+// https://leetcode.com/problems/group-anagrams/description/
+
+function groupAnagrams(strs: string[]): string[][] {
+  return [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+}
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])) // [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+export { }
