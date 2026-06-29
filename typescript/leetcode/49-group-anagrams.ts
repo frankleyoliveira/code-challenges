ec3d9293bf106377c0714361dfae0ceeb123fc9d
Frankley Oliveira <frankleyj.o@gmail.com>
2026-06-29 16:39:30 -0300

    complete 49

--- typescript/leetcode/49-group-anagrams.ts
+++ typescript/leetcode/49-group-anagrams.ts
@@ -2,7 +2,21 @@
 // https://leetcode.com/problems/group-anagrams/description/
 
 function groupAnagrams(strs: string[]): string[][] {
-  return [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+  const result: string[][] = []
+  const groupsMap = new Map<string, number>()
+
+  for (let i = 0; i < strs.length; i++) {
+    const sorted = strs[i].split('').sort().join('')
+    const idx = groupsMap.get(sorted)
+    if (idx !== undefined) {
+      result[idx].push(strs[i])
+    } else {
+      groupsMap.set(sorted, result.length)
+      result.push([strs[i]])
+    }
+  }
+
+  return result
 }
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])) // [["bat"],["nat","tan"],["ate","eat","tea"]]
