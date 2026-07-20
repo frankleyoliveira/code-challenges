d873ed0c8e05d5021c9cebf0a1acd22731315c79
Frankley Oliveira <frankleyj.o@gmail.com>
2026-07-19 21:33:52 -0300

    complete 424 sliding window final

--- typescript/leetcode/424-longest-repeating-character-replacement.ts
+++ typescript/leetcode/424-longest-repeating-character-replacement.ts
@@ -2,23 +2,21 @@
 // https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
 function characterReplacement(s: string, k: number): number {
-  const charMap = new Map<string, number>([[s[0], 1]])
-  let res = 1
+  const countMap = new Map<string, number>()
+  let res = 0
   let l = 0
-  let r = 0
+  let maxF = 0
 
-  while (r < s.length) {
-    const windowLen = r - l + 1
-    const maxFrequency = Math.max(0, Math.max(...charMap.values()))
+  for (let r = 0; r < s.length; r++) {
+    countMap.set(s[r], (countMap.get(s[r]) ?? 0) + 1)
+    maxF = Math.max(maxF, countMap.get(s[r])!)
 
-    if (windowLen - maxFrequency <= k) {
-      res = Math.max(res, windowLen)
-      r++
-      charMap.set(s[r], (charMap.get(s[r]) ?? 0) + 1)
-    } else {
-      charMap.set(s[l], charMap.get(s[l])! - 1)
+    while ((r - l + 1) - maxF > k) {
+      countMap.set(s[l], countMap.get(s[l])! - 1)
       l++
     }
+
+    res = Math.max(res, r - l + 1)
   }
 
   return res
