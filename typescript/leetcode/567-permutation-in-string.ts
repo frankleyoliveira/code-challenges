3b839ba7c692eb287450e24506c2473e9617a217
Frankley Oliveira <frankleyj.o@gmail.com>
2026-07-19 21:44:34 -0300

    init 567

---
+++ typescript/leetcode/567-permutation-in-string.ts
@@ -0,0 +1,11 @@
+// 567. Permutation in String
+// https://leetcode.com/problems/permutation-in-string/description/
+
+function checkInclusion(s1: string, s2: string): boolean {
+  return false
+}
+
+console.log(checkInclusion('ab', 'eidbaooo')) // true
+console.log(checkInclusion('ab', 'eidboaoo')) // false
+
+export { }
