b79c064a73290f45838b4e87707348c09bc2399e
Frankley Oliveira <frankleyj.o@gmail.com>
2026-07-19 21:16:45 -0300

    sliding window v1

--- typescript/leetcode/424-longest-repeating-character-replacement.ts
+++ typescript/leetcode/424-longest-repeating-character-replacement.ts
@@ -2,33 +2,26 @@
 // https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
 function characterReplacement(s: string, k: number): number {
-  let max = 1
+  const charMap = new Map<string, number>([[s[0], 1]])
+  let res = 1
+  let l = 0
+  let r = 0
 
-  for (let i = 0; i < s.length; i++) {
-    let counter = 1
-    let kCount = k
+  while (r < s.length) {
+    const windowLen = r - l + 1
+    const maxFrequency = Math.max(0, Math.max(...charMap.values()))
 
-    for (let j = i + 1; j < s.length; j++) {
-      if (s[j] === s[i]) {
-        counter++
-        max = Math.max(max, counter)
-      } else if (kCount > 0) {
-        kCount--
-        counter++
-        max = Math.max(max, counter)
-      } else {
-        counter = 1
-        kCount = k
-        break
-      }
-    }
-
-    if (kCount > 0) {
-      max = Math.max(max, counter + Math.min(kCount, i))
+    if (windowLen - maxFrequency <= k) {
+      res = Math.max(res, windowLen)
+      r++
+      charMap.set(s[r], (charMap.get(s[r]) ?? 0) + 1)
+    } else {
+      charMap.set(s[l], charMap.get(s[l])! - 1)
+      l++
     }
   }
 
-  return max
+  return res
 }
 
 console.log(characterReplacement("ABAB", 2)) // 4
