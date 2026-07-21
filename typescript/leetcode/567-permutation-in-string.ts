478cb20bb1b59e8888611e11fecd46dbc550bbdd
Frankley Oliveira <frankleyj.o@gmail.com>
2026-07-20 21:40:03 -0300

    complete 567 sliding window

--- typescript/leetcode/567-permutation-in-string.ts
+++ typescript/leetcode/567-permutation-in-string.ts
@@ -4,21 +4,42 @@
 function checkInclusion(s1: string, s2: string): boolean {
   if (s1.length > s2.length) return false
 
-  let left = 0
-  const sortedS1 = s1.split('').sort().join('')
-
-  while (left + s1.length <= s2.length) {
-    const windowStr = s2.slice(left, left + s1.length)
-    const sorted = windowStr.split('').sort().join('')
-    console.log('windowStr', windowStr)
-    console.log('sorted', sorted)
-
-    if (sorted === sortedS1) return true
-
-    left++
+  const s1Count = Array(26).fill(0)
+  const s2Count = Array(26).fill(0)
+  for (let i = 0; i < s1.length; i++) {
+    s1Count[s1.charCodeAt(i) - 97]++
+    s2Count[s2.charCodeAt(i) - 97]++
   }
 
-  return false
+  let matches = 0
+  for (let i = 0; i < 26; i++) {
+    if (s1Count[i] === s2Count[i]) matches++
+  }
+
+  let l = 0
+  for (let r = s1.length; r < s2.length; r++) {
+    if (matches === 26) return true
+
+    const rIdx = s2.charCodeAt(r) - 97
+    s2Count[rIdx]++
+    if (s1Count[rIdx] === s2Count[rIdx]) {
+      matches++
+    } else if (s1Count[rIdx] + 1 === s2Count[rIdx]) {
+      matches--
+    }
+
+    const lIdx = s2.charCodeAt(l) - 97
+    s2Count[lIdx]--
+    if (s1Count[lIdx] === s2Count[lIdx]) {
+      matches++
+    } else if (s1Count[lIdx] - 1 === s2Count[lIdx]) {
+      matches--
+    }
+
+    l++
+  }
+
+  return matches === 26
 }
 
 console.log(checkInclusion('ab', 'eidbaooo')) // true
