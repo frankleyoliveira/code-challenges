782b1cab11e8b380c8b541d4c62f526b46c13033
Frankley Oliveira <frankleyj.o@gmail.com>
2026-07-24 21:25:35 -0300

    init 739

---
+++ typescript/leetcode/739-daily-temperatures.ts
@@ -0,0 +1,16 @@
+// 739. Daily Temperatures
+// https://leetcode.com/problems/daily-temperatures/description/
+
+function dailyTemperatures(temperatures: number[]): number[] {
+  const answer: number[] = []
+
+
+
+  return answer
+}
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])) // [1,1,4,2,1,1,0,0]
+console.log(dailyTemperatures([30, 40, 50, 60])) // [1,1,1,0]
+console.log(dailyTemperatures([30, 60, 90])) // [1,1,0]
+
+export { }
