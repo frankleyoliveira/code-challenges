b67b9bf34b25b16083342e44089b637209c6ac48
Frankley Oliveira <frankleyj.o@gmail.com>
2026-07-27 11:07:50 -0300

    refactor 74

--- typescript/leetcode/74-search-a-2d-matrix.ts
+++ typescript/leetcode/74-search-a-2d-matrix.ts
@@ -2,46 +2,41 @@
 // https://leetcode.com/problems/search-a-2d-matrix/description/
 
 function searchMatrix(matrix: number[][], target: number): boolean {
-  let rowL = 0
-  let rowR = matrix.length - 1
-  let targetRow = -1
+  let t = 0 // top
+  let b = matrix.length - 1 // bottom
+  let row = 0
 
-  while (rowL < rowR && targetRow === -1) {
-    const mid = rowL + Math.floor((rowR - rowL) / 2)
-    if (matrix[mid][0] > target) {
-      rowR = mid - 1
-    } else if (matrix[mid][matrix[mid].length - 1] < target) {
-      rowL = mid + 1
+  while (t <= b) {
+    row = Math.floor((b + t) / 2)
+    if (matrix[row][0] > target) {
+      b = row - 1
+    } else if (matrix[row][matrix[row].length - 1] < target) {
+      t = row + 1
     } else {
-      targetRow = mid
+      break
     }
   }
 
-  if (targetRow === -1) {
-    targetRow = rowL
-  }
+  let l = 0
+  let r = matrix[row].length - 1
 
-  let colL = 0
-  let colR = matrix[targetRow].length - 1
-
-  let count = 0
-  while (colL < colR && count < 10) {
-    const midIdx = colL + Math.floor((colR - colL) / 2)
-    const midVal = matrix[targetRow][midIdx]
+  while (l <= r) {
+    const midIdx = Math.floor((r + l) / 2)
+    const midVal = matrix[row][midIdx]
 
     if (midVal > target) {
-      colR = midIdx - 1
+      r = midIdx - 1
     } else if (midVal < target) {
-      colL = midIdx + 1
+      l = midIdx + 1
     } else {
       return true
     }
-    count++
   }
 
-  return matrix[targetRow][colL] === target
+  return false
 }
 
+console.log(searchMatrix([[1], [3]], 3)) // true
 console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)) // true
 console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13)) // false
 
