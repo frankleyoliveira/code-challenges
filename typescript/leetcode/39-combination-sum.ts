16e82f9ac0eb8b96df23fefdc5409b7a30591c9e
Frankley Oliveira <frankleyj.o@gmail.com>
2026-08-02 19:18:06 -0300

    init 39

---
+++ typescript/leetcode/39-combination-sum.ts
@@ -0,0 +1,14 @@
+// 39. Combination Sum
+// https://leetcode.com/problems/combination-sum/description/
+
+function combinationSum(candidates: number[], target: number): number[][] {
+  const result: number[][] = []
+
+  return result
+}
+
+console.log(combinationSum([2, 3, 6, 7], 7)) // [[2,2,3],[7]]
+console.log(combinationSum([2, 3, 5], 8)) // [[2,2,2,2],[2,3,3],[3,5]]
+console.log(combinationSum([2], 1)) // []
+
+export { }
