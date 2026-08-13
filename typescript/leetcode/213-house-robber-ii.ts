feda5c12dfaa0ea074515761412d4f519e638985
Frankley Oliveira <frankleyj.o@gmail.com>
2026-08-13 10:34:28 -0300

    213 refactor

--- typescript/leetcode/213-house-robber-ii.ts
+++ typescript/leetcode/213-house-robber-ii.ts
@@ -2,32 +2,24 @@
 // https://leetcode.com/problems/house-robber-ii/description/
 
 function rob(nums: number[]): number {
-  if (nums.length === 1) return nums[0]
-  if (nums.length === 2) return Math.max(nums[0], nums[1])
-  if (nums.length === 3) return Math.max(nums[0], nums[1], nums[2])
-
-  // robbing first house path
-  let prevPrev = nums[0] // 2
-  let prev = nums[2] + nums[0] // 4
-
-  for (let i = 3; i < nums.length - 1; i++) {
-    const sum = Math.max(nums[i] + prevPrev, prev)
-    prevPrev = prev
-    prev = sum
+  if (nums.length <= 3) {
+    return Math.max(...nums)
   }
 
-  let moneyRobbingFirst = prev
+  const calcMoney = (firstIdx: number, lastIdx: number): number => {
+    let prevPrev = 0
+    let prev = 0
 
-  // skip first house path
-  prevPrev = nums[1]
-  prev = Math.max(nums[1], nums[2])
-  for (let i = 3; i < nums.length; i++) {
-    const sum = Math.max(nums[i] + prevPrev, prev)
-    prevPrev = prev
-    prev = sum
+    for (let i = firstIdx; i <= lastIdx; i++) {
+      const sum = Math.max(nums[i] + prevPrev, prev)
+      prevPrev = prev
+      prev = sum
+    }
+
+    return prev
   }
 
-  return Math.max(moneyRobbingFirst, prev)
+  return Math.max(calcMoney(0, nums.length - 2), calcMoney(1, nums.length - 1))
 }
 
 console.log(rob([1, 2, 1, 1])) // 3
